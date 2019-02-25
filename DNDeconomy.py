b7ffe17e5dbdf7100a897d4e21ff91e3ad1b9a1f
Luke <lukejodonnell@gmail.com>
2019-02-25 01:29:30 -0500

    first commit

---
+++ DNDeconomy.py
@@ -0,0 +1,10 @@
+import Tkinter
+import sys
+print sys.version_info
+print sys.hexversion
+
+if sys.hexversion == (34017217):
+    print "your version is both out of date and the one this was written in"
+
+print "hello"
+
