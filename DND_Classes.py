9a0eb32a4da484357618f3a9b5c2bd87f854dc73
Ian <ian.commins@gordon.edu>
2019-02-27 20:39:36 -0500

    updated classes to include merchants, locations, regions, and rarity

--- DND_Classes.py
+++ DND_Classes.py
@@ -1,5 +1,29 @@
 class Items:
 
-    def __init__(self, name, value):
-        self.name = name
-        self.value = value
+    def __init__(self, item_name, item_value):
+        self.name = item_name
+        self.value = item_value
+
+class Regions:
+
+    def __init__(self, region_name, region_value):
+        self.name = region_name
+        self.region_value = region_value
+
+class Locations:
+
+    def __init__(self, location_type, location_value):
+        self.location_type = location_type
+        self.location_value = location_value
+
+class Merchants:
+
+    def __init__(self, merchant_type, merchant_value):
+        self.merchant_type = merchant_type
+        self.merchant_value = merchant_value
+
+class Rarity:
+
+    def __init__(self, rarity_type, rarity_value):
+        self.rarity_type = rarity_type
+        self.rarity_value = rarity_value
