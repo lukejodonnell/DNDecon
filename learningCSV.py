b64c29e6f01bacbddfed112b177178875154ca25
Luke <lukejodonnell@gmail.com>
2019-02-28 00:22:56 -0500

    Ian is Dumb so I did this

--- learningCSV.py
+++ learningCSV.py
@@ -1,8 +1,8 @@
 import tkinter as tk
 import csv
 
-root = tk.Tk()
-frame = tk.Frame(root)
+#root = tk.Tk()
+#frame = tk.Frame(root)
 
 def printDatCost(messageToPrint):
     print(messageToPrint)
@@ -11,16 +11,16 @@ with open('Weapons.csv', newline='') as csvfile:
      reader = csv.DictReader(csvfile)
      for row in reader:
          print(row['Weapon'], row['Cost'])
-         tk.Radiobutton(frame, 
-                  text=row["Weapon"],
-                  padx = 20, 
-                  command=printDatCost(row['cost'],
-                  indicatoron = 0,
-                  width = 15,
-                  value=val).pack()
-        
+#         tk.Radiobutton(frame, 
+#                  text=row["Weapon"],
+#                  padx = 20, 
+#                  command=printDatCost(row['cost'],
+#                  indicatoron = 0,
+#                  width = 15,
+#                  value=val).pack()
+#        
 
 
-print("help")
-frame.pack()
-root.mainloop()
+#print("help")
+#frame.pack()
+#root.mainloop()
