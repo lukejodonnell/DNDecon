1025be6c067ff881881263eb6841145031792972
Luke <lukejodonnell@gmail.com>
2019-02-28 00:53:00 -0500

    I made the CSVlearning do things and now it is time for bed

--- learningCSV.py
+++ learningCSV.py
@@ -1,26 +1,35 @@
 import tkinter as tk
 import csv
 
-#root = tk.Tk()
-#frame = tk.Frame(root)
+root = tk.Tk()
+frame = tk.Frame(root)
+#I am so mad that I can't figure out how to make this work without this global variable
+weaponCostWhichIsUnfortunatelyAGlobalVariable = "not yet set"
 
-def printDatCost(messageToPrint):
-    print(messageToPrint)
+def printDatCost():
+    global weaponCostWhichIsUnfortunatelyAGlobalVariable
+    print(weaponCostWhichIsUnfortunatelyAGlobalVariable)
 
-with open('Weapons.csv', newline='') as csvfile:
-     reader = csv.DictReader(csvfile)
-     for row in reader:
-         print(row['Weapon'], row['Cost'])
-#         tk.Radiobutton(frame, 
-#                  text=row["Weapon"],
-#                  padx = 20, 
-#                  command=printDatCost(row['cost'],
-#                  indicatoron = 0,
-#                  width = 15,
-#                  value=val).pack()
-#        
+csvfile = open('Weapons.csv', newline='')
 
 
-#print("help")
-#frame.pack()
-#root.mainloop()
+reader = csv.DictReader(csvfile)
+for row in reader:
+    print(row['Weapon'], row['Cost'])
+    weaponCostWhichIsUnfortunatelyAGlobalVariable = row['Cost']
+    tk.Radiobutton(frame, 
+                   text=row["Weapon"],
+                   padx = 20, 
+                   command=printDatCost,
+                   indicatoron = 0,
+                   width = 15,
+                   value=row).pack()
+        
+
+
+print("help")
+frame.pack()
+root.mainloop()
+
+
+#when we last saw our hero on 2019-2-28 the problem I was having was that the buttons don't like, print their cost, odd
